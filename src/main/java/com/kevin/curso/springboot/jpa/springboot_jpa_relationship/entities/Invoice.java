f796e6edbbc16ea9689b7b57922465411f5999e6
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-07-16 10:31:54 -0500

    Feat(JPA): Creando proeyecto y entities

---
+++ src/main/java/com/kevin/curso/springboot/jpa/springboot_jpa_relationship/entities/Invoice.java
@@ -0,0 +1,52 @@
+package com.kevin.curso.springboot.jpa.springboot_jpa_relationship.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "invoices")
+public class Invoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Long total;
+
+    public Invoice() {
+    }
+
+    public Invoice(String description, Long total) {
+        this.description = description;
+        this.total = total;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+}
