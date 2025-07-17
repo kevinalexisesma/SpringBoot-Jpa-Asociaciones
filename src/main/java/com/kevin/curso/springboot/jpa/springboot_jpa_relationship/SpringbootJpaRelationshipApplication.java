5f559a1c397bcbeb4a47b765b272c7af5543f11c
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-07-17 10:54:14 -0500

    Feat(Asociaciones): Codificando ejemplo ManyToOne

--- src/main/java/com/kevin/curso/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
+++ src/main/java/com/kevin/curso/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
@@ -1,13 +1,41 @@
 package com.kevin.curso.springboot.jpa.springboot_jpa_relationship;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.kevin.curso.springboot.jpa.springboot_jpa_relationship.entities.Client;
+import com.kevin.curso.springboot.jpa.springboot_jpa_relationship.entities.Invoice;
+import com.kevin.curso.springboot.jpa.springboot_jpa_relationship.repositories.ClientRepository;
+import com.kevin.curso.springboot.jpa.springboot_jpa_relationship.repositories.InvoiceRepository;
+
 @SpringBootApplication
-public class SpringbootJpaRelationshipApplication {
+public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
+
+	@Autowired
+	private ClientRepository clientRepository;
+
+	@Autowired
+	private InvoiceRepository invoiceRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaRelationshipApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		manyToOne();
+	}
+
+	public void manyToOne() {
+		Client client = new Client("John", "Doe");
+		clientRepository.save(client);
+
+		Invoice invoice = new Invoice("Compras de oficina", 2000L);
+		invoice.setClient(client);
+
+		Invoice invoiceDB = invoiceRepository.save(invoice);
+		System.out.println("Factura guardada: " + invoiceDB);
+	}
 }
--- src/main/java/com/kevin/curso/springboot/jpa/springboot_jpa_relationship/entities/Client.java
+++ src/main/java/com/kevin/curso/springboot/jpa/springboot_jpa_relationship/entities/Client.java
@@ -49,4 +49,9 @@ public class Client {
         this.lastname = lastname;
     }
 
+    @Override
+    public String toString() {
+        return "{id=" + id + ", name=" + name + ", lastname=" + lastname + "}";
+    }
+
 }
