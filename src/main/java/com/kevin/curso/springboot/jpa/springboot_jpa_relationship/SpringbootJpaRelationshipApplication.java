00724b40cbf684b18f130fd6152f0b0b7099ca52
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-07-17 11:13:03 -0500

    Feat(Asociaciones): Codificando ejemplo ManyToOne con relacion existente

--- src/main/java/com/kevin/curso/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
+++ src/main/java/com/kevin/curso/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
@@ -1,5 +1,7 @@
 package com.kevin.curso.springboot.jpa.springboot_jpa_relationship;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -25,7 +27,7 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		manyToOne();
+		manyToOneFindByIdClient();
 	}
 
 	public void manyToOne() {
@@ -37,5 +39,18 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
 
 		Invoice invoiceDB = invoiceRepository.save(invoice);
 		System.out.println("Factura guardada: " + invoiceDB);
+	}
+
+	public void manyToOneFindByIdClient() {
+		Optional<Client> optionalClient = clientRepository.findById(1L);
+
+		if (optionalClient.isPresent()) {
+			Client client = optionalClient.orElseThrow();
+
+			Invoice invoice = new Invoice("Compras de oficina", 2000L);
+			invoice.setClient(client);
+			Invoice invoiceDB = invoiceRepository.save(invoice);
+			System.out.println("Factura guardada: " + invoiceDB);
+		}
 	}
 }
