d1b1abdf9a33860ff9061162ef31b70a67bf8679
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-07-22 09:37:27 -0500

    Feat(asociaciones): Eliminando hijos en la relación OneToMany

--- src/main/java/com/kevin/curso/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
+++ src/main/java/com/kevin/curso/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
@@ -31,7 +31,28 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		OneToManyFindById();
+		removeAddress();
+	}
+
+	@Transactional
+	public void removeAddress() {
+		Client client = new Client("Fran", "Moras");
+		Address address1 = new Address("El Verjel", 1234);
+		Address address2 = new Address("Vasco de Gama", 9875);
+
+		client.getAddresses().add(address1);
+		client.getAddresses().add(address2);
+
+		clientRepository.save(client);
+
+		System.out.println(client);
+
+		Optional<Client> optionalClient = clientRepository.findById(3L);
+		optionalClient.ifPresent(c -> {
+			c.getAddresses().remove(address1);
+			clientRepository.save(c);
+			System.out.println(c);
+		});
 	}
 
 	@Transactional
